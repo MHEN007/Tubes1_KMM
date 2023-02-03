863396aa042dff9535c74d2a41f7a64fcc6146cf
MHEN2606 <matthew.mahendra@gmail.com>
2023-02-03 10:43:40 +0700

    Greedy By Position Scheme

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -36,15 +36,64 @@ public class BotService {
     public void computeNextPlayerAction(PlayerAction playerAction) {
         playerAction.action = PlayerActions.FORWARD;
         playerAction.heading = new Random().nextInt(360);
+        System.out.println("Waktunya jalan ke depan");
 
-        if (!gameState.getGameObjects().isEmpty()) {
-            var foodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
-                    .collect(Collectors.toList());
+        if(!this.gameState.getGameObjects().isEmpty()){
+            /* GREEDY BY POSITION */
+            
+            /* Kemungkinan Posisi Pertama: Dekat dengan edge 
+             * Langkah yang diambil adalah putar balik lalu maju
+            */
+            int x = bot.getPosition().getX();
+            int y = bot.getPosition().getY();
 
-            playerAction.heading = getHeadingBetween(foodList.get(0));
+            double jarakKePusat = Math.sqrt(x*x + y*y);
+
+            if(jarakKePusat + (1.5 * bot.getSize()) >= gameState.getWorld().getRadius()){
+                System.out.println("Waktunya putar balik!");
+                playerAction.heading = (playerAction.heading + 180) % 360;
+            }
+
+            /* Kemungkinan Posisi Kedua: Dekat dengan obstacle 
+             * Langkah yang diambil adalah menghindari objek tsb.
+             * Entah itu belok kanan, kiri, mundur
+            */
+
+            var listOfObstacles = gameState.getGameObjects()
+                            .stream().filter(obstacle -> obstacle.getGameObjectType() == ObjectTypes.GAS_CLOUD || obstacle.getGameObjectType() == ObjectTypes.ASTEROID_FIELD)
+                            .sorted(Comparator.comparing(obstacle -> getDistanceBetween(bot, obstacle)))
+                            .collect(Collectors.toList());
+            
+            double jarakObjKePusat = Math.sqrt(listOfObstacles.get(0).getPosition().getX() * listOfObstacles.get(0).getPosition().getX() + listOfObstacles.get(0).getPosition().getY() + listOfObstacles.get(0).getPosition().getY());
+            if(jarakKePusat + (1.5 * bot.getSize()) == jarakObjKePusat){
+                System.out.println("Waktunya menghindari Objek!");
+                playerAction.heading = (playerAction.heading + 180)%360;
+            }
+
+            /* Kemungkinan Posisi Ketiga: Dekat dengan Supernova, food, dan wormhole
+             * Langkah yang diambil: 
+             * Kalau ukuran diri > 10 (ambil batas aman) Boleh semuanya
+             * Kalau ukuran diri < 10, ambil supernova dan food aja. Jangan ke wormhole
+            */
+
+            var friendlyObjects = gameState.getGameObjects()
+                            .stream().filter(friendly -> friendly.getGameObjectType() == ObjectTypes.FOOD 
+                                                || friendly.getGameObjectType() == ObjectTypes.SUPER_FOOD 
+                                                || friendly.getGameObjectType() == ObjectTypes.SHIELD
+                                                || friendly.getGameObjectType() == ObjectTypes.SUPERNOVA_PICKUP
+                                                || friendly.getGameObjectType() == ObjectTypes.WORMHOLE
+                            )
+                            .sorted(Comparator.comparing(obstacle -> getDistanceBetween(bot, obstacle)))
+                            .collect(Collectors.toList());
+
+            if(bot.getSize() < 10){
+                var filterWormhole = friendlyObjects.stream().filter(filtWormhole -> filtWormhole.getGameObjectType() != ObjectTypes.WORMHOLE)
+                            .sorted(Comparator.comparing(filtWormhole -> getDistanceBetween(bot, filtWormhole)))
+                            .collect(Collectors.toList());
+                playerAction.heading = getHeadingBetween(filterWormhole.get(0));
+            }else{
+                playerAction.heading = getHeadingBetween(friendlyObjects.get(0));
+            }
         }
 
         this.playerAction = playerAction;
