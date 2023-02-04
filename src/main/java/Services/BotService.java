3dc69e435407673f4d07526c5448e10d638be459
MHEN2606 <matthew.mahendra@gmail.com>
2023-02-04 09:54:27 +0700

    Added flags

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -10,6 +10,8 @@ public class BotService {
     private GameObject bot;
     private PlayerAction playerAction;
     private GameState gameState;
+    private int decisionFlag = 0;
+    private GameObject temp_target, temp_player;
 
     public BotService() {
         this.playerAction = new PlayerAction();
@@ -36,110 +38,162 @@ public class BotService {
     public void computeNextPlayerAction(PlayerAction playerAction) {
         playerAction.action = PlayerActions.FORWARD;
         playerAction.heading = new Random().nextInt(360);
-        System.out.println("Waktunya jalan ke depan");
+        System.out.println("TIME TO COMPUTE");
 
         if(!this.gameState.getGameObjects().isEmpty()){
-            /* GREEDY BY POSITION */
-            GameObject worldCenter = new GameObject(null, null, null, null, new Position(), null, null);
-            int x = bot.getPosition().getX();
-            int y = bot.getPosition().getY();
-
-            double jarakKePusat = Math.sqrt(x*x + y*y);
-
-            /* Kemungkinan Posisi Pertama: Dekat dengan Supernova, food, dan wormhole
-            * Langkah yang diambil: 
-            * Object diambil kalau jaraknya < 5
-            * Kalau ada wormhole dan cukup, ambil
-            * SIsanya ambil aja barang2
-            */
-            var friendlyObjects = gameState.getGameObjects()
-                    .stream().filter(friendly -> friendly.getGameObjectType() == ObjectTypes.FOOD 
-                                        || friendly.getGameObjectType() == ObjectTypes.SUPER_FOOD 
-                                        || friendly.getGameObjectType() == ObjectTypes.SHIELD
-                                        || friendly.getGameObjectType() == ObjectTypes.SUPERNOVA_PICKUP
-                                        || friendly.getGameObjectType() == ObjectTypes.WORMHOLE
-                    )
-                    .sorted(Comparator.comparing(friendly -> getDistanceBetween(bot, friendly)))
-                    .collect(Collectors.toList());
-
-            if(getDistanceBetween(bot, friendlyObjects.get(0)) < 5){
-                if(friendlyObjects.get(0).getGameObjectType() != ObjectTypes.WORMHOLE){
-                    playerAction.heading = getHeadingBetween(friendlyObjects.get(0));
-                    System.out.println("Cari item yuk!");
-                } else if (friendlyObjects.get(0).getGameObjectType() == ObjectTypes.WORMHOLE && friendlyObjects.get(0).size < bot.getSize()){
-                    playerAction.heading = getHeadingBetween(friendlyObjects.get(0));
-                    System.out.println("Masuk Wormhole yuk!");
-                }
-            }
-
-            /* Kemungkinan Posisi Kedua: Dekat dengan Player Lain
-            * Langkah yang diambil: Periksa dulu size diri dan size musuh
-            * Kalau size diri > size player lain: Serang player terdekat
-            * Kalau size diri < size player lain: Menghindar dl aja, makan food dl
-            */
             var nearOtherPlayer = gameState.getGameObjects()
-                        .stream().filter(otherPlayer -> otherPlayer.getId() != bot.getId())
-                        .sorted(Comparator.comparing(otherPlayer -> getDistanceBetween(bot, otherPlayer)))
-                        .collect(Collectors.toList());
-            
-            if(bot.getSize() <= nearOtherPlayer.get(0).getSize() && getDistanceBetween(bot, nearOtherPlayer.get(0)) < 30 ) {
-                System.out.println("Waktunya menghindari musuh!");
-                playerAction.heading = (nearOtherPlayer.get(0).currentHeading + 45) % 360;
-                System.out.println("Turn on flag");
+                            .stream().filter(otherPlayer -> otherPlayer.getId() != bot.getId())
+                            .sorted(Comparator.comparing(otherPlayer -> getDistanceBetween(bot, otherPlayer)))
+                            .collect(Collectors.toList());
+            if(decisionFlag == 0){
+                /* GREEDY BY POSITION */
+                GameObject worldCenter = new GameObject(null, null, null, null, new Position(), null, null);
 
-                /* Buat pertimbangan pakai shield kalau ada */
-            } else if (bot.getSize() > nearOtherPlayer.get(0).getSize() && getDistanceBetween(bot, nearOtherPlayer.get(0)) < 30){
-                System.out.println("Waktunya menyerang musuh!");
-                playerAction.heading = getHeadingBetween(nearOtherPlayer.get(0));
-                /* Buat pertimbangan pakai weapon kalau jaraknya kejauhan */
-            }
+                double jarakKePusat = getDistanceBetween(bot, worldCenter);
 
-            /* Kemungkinan Posisi Ketiga: Dekat dengan obstacle 
-            * Langkah yang diambil adalah menghindari objek tsb.
-            * Entah itu belok kanan, kiri, mundur
-            */
-            var listOfObstacles = gameState.getGameObjects()
-                        .stream().filter(obstacle -> obstacle.getGameObjectType() == ObjectTypes.GAS_CLOUD || obstacle.getGameObjectType() == ObjectTypes.ASTEROID_FIELD)
-                        .sorted(Comparator.comparing(obstacle -> getDistanceBetween(bot, obstacle)))
-                        .collect(Collectors.toList());
-
-            double jarakObjKePusat = Math.sqrt(listOfObstacles.get(0).getPosition().getX() * listOfObstacles.get(0).getPosition().getX() + listOfObstacles.get(0).getPosition().getY() + listOfObstacles.get(0).getPosition().getY());
-            if(jarakKePusat + (0.5 * bot.getSize()) == jarakObjKePusat){
-                System.out.println("Waktunya menghindari Objek!");
-                playerAction.heading = getOppositeDirection(bot, listOfObstacles.get(0));
-            }
-
-            /* Kemungkinan Posisi Torpedo Salvo 
-            * Langkah yang diambil: Mendeteksi torpedo salvo dari musuh
-            * Kalau sedang ada di lintasan torpedo salve : Menghindar dari torpedo salvo
-            */
-            var detectTorpedo = gameState.getGameObjects()
-                        .stream().filter(SALVO-> SALVO.getGameObjectType() == ObjectTypes.TORPEDO_SALVO)
-                        .sorted(Comparator.comparing(SALVO -> getDistanceBetween(bot, SALVO)))
+                /* Kemungkinan Posisi Pertama: Dekat dengan Supernova, food, dan wormhole
+                * Langkah yang diambil: 
+                * Kalau ada wormhole dan cukup, ambil
+                * SIsanya ambil aja barang2
+                */
+                var friendlyObjects = gameState.getGameObjects()
+                        .stream().filter(friendly -> friendly.getGameObjectType() == ObjectTypes.FOOD 
+                                            || friendly.getGameObjectType() == ObjectTypes.SUPER_FOOD 
+                                            || friendly.getGameObjectType() == ObjectTypes.SHIELD
+                                            || friendly.getGameObjectType() == ObjectTypes.SUPERNOVA_PICKUP
+                                            || friendly.getGameObjectType() == ObjectTypes.WORMHOLE
+                        )
+                        .sorted(Comparator.comparing(friendly -> getDistanceBetween(bot, friendly)))
                         .collect(Collectors.toList());
 
-            if(detectTorpedo.size() != 0){
-                double jarakTorpedoKePusat = Math.sqrt(detectTorpedo.get(0).getPosition().getX() * detectTorpedo.get(0).getPosition().getX() + detectTorpedo.get(0).getPosition().getY() + detectTorpedo.get(0).getPosition().getY());
-                
-                if(jarakKePusat + (1.5 * bot.getSize()) == jarakTorpedoKePusat){
-                    if(bot.getSize() < 50){
-                        System.out.println("Ada salvo! Pergi!");
-                        playerAction.heading = (playerAction.heading + 180)%360;
+                if(getDistanceBetween(worldCenter, friendlyObjects.get(0)) + (1.5 * bot.getSize()) < gameState.getWorld().getRadius()){
+                    if(friendlyObjects.get(0).getGameObjectType() != ObjectTypes.WORMHOLE){
+                        playerAction.heading = getHeadingBetween(friendlyObjects.get(0));
+                        decisionFlag = 2; // kejer
+                        temp_target = friendlyObjects.get(0);
+                        System.out.println("Cari item yuk!");
+                        System.out.println(playerAction.heading);
+                    } else if (friendlyObjects.get(0).getGameObjectType() == ObjectTypes.WORMHOLE && friendlyObjects.get(0).size < bot.getSize()){
+                        playerAction.heading = getHeadingBetween(friendlyObjects.get(0));
+                        System.out.println("Masuk Wormhole yuk!");
                     }
-                    else {
-                        System.out.println("Ada salvo! Pasang shield");
-                        //masang shield
+                } else {
+                    System.out.println("Leave item");
+                    playerAction.heading = getHeadingBetween(worldCenter);
+                }
+
+                /* Kemungkinan Posisi Kedua: Dekat dengan Player Lain
+                * Langkah yang diambil: Periksa dulu size diri dan size musuh
+                * Kalau size diri > size player lain: Serang player terdekat
+                * Kalau size diri < size player lain: Menghindar dl aja, makan food dl
+                */
+                
+                if(bot.getSize() <= nearOtherPlayer.get(0).getSize() && getDistanceBetween(bot, nearOtherPlayer.get(0)) < 30 ) {
+                    System.out.println("Waktunya menghindari musuh!");
+                    playerAction.heading = getOppositeDirection(bot, nearOtherPlayer.get(0));
+                    System.out.println("Turn on flag");
+                    decisionFlag = 1; // kabur flag
+                    temp_player = nearOtherPlayer.get(0);
+                    /* Buat pertimbangan pakai shield kalau ada */
+                }
+                
+                if (bot.getSize() > nearOtherPlayer.get(0).getSize() && getDistanceBetween(bot, nearOtherPlayer.get(0)) < 30){
+                    System.out.println("Waktunya menyerang musuh!");
+                    playerAction.heading = getHeadingBetween(nearOtherPlayer.get(0));
+                    decisionFlag = 3;
+                    temp_player = nearOtherPlayer.get(0);
+                    /* Buat pertimbangan pakai weapon kalau jaraknya kejauhan */
+                }
+
+                /* Kemungkinan Posisi Ketiga: Dekat dengan obstacle 
+                * Langkah yang diambil adalah menghindari objek tsb.
+                * Entah itu belok kanan, kiri, mundur
+                */
+                var listOfObstacles = gameState.getGameObjects()
+                            .stream().filter(obstacle -> obstacle.getGameObjectType() == ObjectTypes.GAS_CLOUD || obstacle.getGameObjectType() == ObjectTypes.ASTEROID_FIELD)
+                            .sorted(Comparator.comparing(obstacle -> getDistanceBetween(bot, obstacle)))
+                            .collect(Collectors.toList());
+
+                double jarakObjKePusat = getDistanceBetween(listOfObstacles.get(0), worldCenter);
+                if(jarakKePusat + (0.5 * bot.getSize()) == jarakObjKePusat){
+                    System.out.println("Waktunya menghindari Objek!");
+                    playerAction.heading = getOppositeDirection(bot, listOfObstacles.get(0));
+                    decisionFlag = 1;
+                    temp_player = listOfObstacles.get(0);
+                }
+
+                /* Kemungkinan Posisi Torpedo Salvo 
+                * Langkah yang diambil: Mendeteksi torpedo salvo dari musuh
+                * Kalau sedang ada di lintasan torpedo salve : Menghindar dari torpedo salvo
+                */
+                var detectTorpedo = gameState.getGameObjects()
+                            .stream().filter(SALVO-> SALVO.getGameObjectType() == ObjectTypes.TORPEDO_SALVO)
+                            .sorted(Comparator.comparing(SALVO -> getDistanceBetween(bot, SALVO)))
+                            .collect(Collectors.toList());
+
+                if(detectTorpedo.size() != 0){
+                    double jarakTorpedoKePusat = getDistanceBetween(detectTorpedo.get(0), worldCenter);
+
+                    if(jarakKePusat + (1.5 * bot.getSize()) == jarakTorpedoKePusat){
+                        if(bot.getSize() < 50){
+                            System.out.println("Ada salvo! Pergi!");
+                            playerAction.heading = (playerAction.heading + 180)%360;
+                        }
+                        else {
+                            System.out.println("Ada salvo! Pasang shield");
+                            //masang shield
+                        }
                     }
                 }
-            }
 
-            /* Kemungkinan Posisi Pertama: Dekat dengan edge 
-            * Langkah yang diambil adalah putar balik lalu maju
-            */
+                /* Kemungkinan Posisi Pertama: Dekat dengan edge 
+                * Langkah yang diambil adalah putar balik lalu maju
+                */
 
-            if(jarakKePusat + (1.5 * bot.getSize()) >= gameState.getWorld().getRadius()){
-                System.out.println("Waktunya putar balik!");
-                playerAction.heading = getHeadingBetween(worldCenter);
+                if(jarakKePusat + (1.5 * bot.getSize()) >= gameState.getWorld().getRadius()){
+                    System.out.println("Waktunya putar balik!");
+                    playerAction.heading = getHeadingBetween(worldCenter);
+                }
+            } else if (decisionFlag == 1) {
+                /* Cek apakah obstacle masih di sekitar bot atau tidak */
+                /* Kalau masih ada, headingnya masih sama */
+                /* Kalau tidak flag matikan */
+                if((nearOtherPlayer.get(0) == temp_player && nearOtherPlayer.get(0).getSize() >= bot.getSize()) || (temp_player.getPosition() == bot.getPosition())){
+                    System.out.println("KEEP RUNNING!");
+                    playerAction.heading = getOppositeDirection(bot, temp_player);
+                }else{
+                    decisionFlag = 0;
+                    temp_player = null;
+                    System.out.println("You're safe, for now");
+                }
+            } else if (decisionFlag == 2) {
+                /* Cek apakah objek masih ada. Kalau masih ada kejer terus objeknya
+                 * Kalau udah gak ada abandon. Balikin flag jadi 0
+                 */
+                playerAction.heading = getHeadingBetween(temp_target);
+                if(!gameState.getGameObjects().contains(temp_target))
+                {
+                    temp_target = null;
+                    decisionFlag = 0;
+                    System.out.println("Item doesn't exist anymore. Abandon!");
+                } else {
+                    System.out.println("Item still exist. KEJAR!");
+                }
+                
+                System.out.println(playerAction.heading);
+            } else if (decisionFlag == 3) {
+                /* Cek apakah masih dekat atau tidak dan ukuran masih aman atau gak 
+                 * Kalau aman serang
+                 * Kalau gak aman kabur, puter 180 derajat aja
+                */
+                if(getDistanceBetween(temp_player, bot) < 30 && temp_player.getSize() < bot.getSize()){
+                    playerAction.heading = getHeadingBetween(temp_player);
+                    System.out.println("SERANG");
+                }else{
+                    decisionFlag = 0;
+                    playerAction.heading = getOppositeDirection(temp_player, bot);
+                    System.out.println("RETREAT");
+                }
             }
         }
 
